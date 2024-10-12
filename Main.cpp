c4f2a74e179e153947bf4a83d9da7243b481e921
Your Name <your-email@example.com>
2024-10-12 07:07:18 -0700

    update

--- Main.cpp
+++ Main.cpp
@@ -213,24 +213,17 @@ public:
         }
     }
 };
+ 
+//Protoype
+int get_random_list_size();
 
 // Driver program
 int main() {
-    DoublyLinkedList list;
-    int size = rand() % (MAX_LS - MIN_LS + 1) + MIN_LS;
-
-    for (int i = 0; i < size; ++i)
-        list.push_back(rand() % (MAX_NR - MIN_NR + 1) + MIN_NR);
-    cout << "List forward: ";
-    list.print();
-
-    cout << "List backward: ";
-    list.print_reverse();
-
-    cout << "Deleting list, then trying to print.\n";
-    list.~DoublyLinkedList();
-    cout << "List forward: ";
-    list.print();
+  
 
     return 0;
+}
+
+int get_random_list_size() {
+
 }
